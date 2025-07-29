b4e558eb8e092a7c79fcb94d7bb4ace856bca1a7
oscar-mario-gonzalez <oscar-mario.gonzalez@hpe.com>
2025-07-29 10:23:17 -0600

    Refactor code to use iterators according to The Rust Programming Language

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::{env, process};
 use tooly::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let configuration = Config::build(&args).unwrap_or_else(|err| {
+    let configuration = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
